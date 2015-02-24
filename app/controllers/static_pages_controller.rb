4c4c50a63f7cdae77a4eb428027aa4a0572e85ef
Ujjwal Jung Pandey <ujpandey1@cougars.ccis.edu>
2015-02-24 12:27:20 -0600

    Add a Faye demo

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -16,6 +16,23 @@ class StaticPagesController < ApplicationController
   def match
     @p1 = Ai.find(params[:p1_id])
     @p2 = Ai.find(params[:champion])
+    @p1_path = @p1.location.current_path
+    @p2_path = @p2.location.current_path
+    @state = "         "
+
+    client = Faye::Client.new('/faye')
+
+    while True do
+    RubyPython.start  
+    sys = RubyPython.import("sys")
+    sys.path.append()
+    
+    test = RubyPython.import("get_move")
+    @text = test.get_move(params[:move][:match_state], params[:move][:time_left]).rubify 
+
+    RubyPython.stop
+    end
+    
   end
   
 end
